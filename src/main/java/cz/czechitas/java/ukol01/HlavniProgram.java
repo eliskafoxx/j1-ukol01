d63d1b2be8daf696cb6b753b1f98ff945b39772f
eliskafoxx <eliska.foxx@gmail.com>
2023-02-18 13:35:50 +0100

    prase stopka kruh barak

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -2,8 +2,7 @@ package cz.czechitas.java.ukol01;
 
 import cz.czechitas.java.ukol01.engine.*;
 
-import static java.awt.Color.pink;
-import static java.awt.Color.red;
+import static java.awt.Color.*;
 
 public class HlavniProgram {
 
@@ -14,6 +13,11 @@ public class HlavniProgram {
     public void NakresliPrasatko() {
         Turtle zofka = new Turtle();
         zofka.setPenColor(pink);
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(75);
+        zofka.turnRight(90);
+        zofka.penDown();
         //nozka
         zofka.turnRight(210);
         zofka.move(30);
@@ -78,8 +82,56 @@ public class HlavniProgram {
             }
         }
 
+    public void Slunce() {
+        Turtle zofka = new Turtle();
+        zofka.penUp();
+        zofka.move(200);
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.penDown();
+        zofka.setPenColor(yellow);
+
+        for (int i = 0; i < 18; i++) {
+            zofka.move(15);
+            zofka.turnRight(20);
+
+        }
+
+    }
+
+    public void Barak() {
+        Turtle zofka = new Turtle();
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnRight(90);
+        zofka.move(125);
+        zofka.penDown();
+        zofka.setPenColor(black);
+        zofka.penDown();
+
+        //barak
+        zofka.turnRight(180);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(90);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(90);
+        zofka.turnLeft(135);
+        zofka.move(65);
+        zofka.turnLeft(90);
+        zofka.move(65);
+
+
+
+
+    }
+
     public static void main(String[] args) {
-        new HlavniProgram().Kruh();
+        new HlavniProgram().Barak();
     }
 
 
